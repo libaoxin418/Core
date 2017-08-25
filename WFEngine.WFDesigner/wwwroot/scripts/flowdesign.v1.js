81e7ae69f3d5a0190d7255cf9a94e04d20b4f74b
李宝鑫 <libaoxin131521@hotmail.com>
2017-08-25 18:54:46 +0800

    G

---
+++ WFEngine.WFDesigner/wwwroot/scripts/flowdesign.v1.js
@@ -0,0 +1,27 @@
+﻿var FlowDesign = {
+    init: function () {
+        var workflowId = Utility.GetQueryString("wfId");
+        $.ajax({
+            url: "/api/workflow/wfDesigner/design?wfid=" + workflowId,
+            type: "Get",
+            success: function (result) {
+                DispWorkflowDesign(result);
+            }
+        });
+    },
+
+    DispWorkflowDesign: function (result) { },
+
+    SaveWorkflowDesign: function () { },
+
+
+};
+
+
+var Utility = {
+    GetQueryString: function (name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return unescape(r[2]); return null;
+    }
+}
